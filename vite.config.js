3756cf45779a0cb664f039c7645149260ff78f61
unknown <azamazizi@gmail.com>
2026-01-24 13:50:37 +0800

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/dropship-apps/" : "/",
+  base: "/dropship-apps/",
 
   plugins: [
     vue(),
